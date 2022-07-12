68a03f1bc6da7cbbc521faaea6ff022d65376859
CoopTRUE <coopdog4545@gmail.com>
2022-07-11 22:43:11 -0700

    pretty sure vite compression plugin not needed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), viteCompression()],
+  plugins: [svelte()]
 })
